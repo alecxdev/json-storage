41b2317813597ecd679574965eaa524e8c9b873b
Alejandro <alexchuc84@gmail.com>
2024-09-26 18:00:31 -0600

    Add get collection list and get collection by id

--- src/router/collections.ts
+++ src/router/collections.ts
@@ -3,12 +3,19 @@ import json from '../db/data.json';
 import { BadRequestError } from '../exceptions';
 
 const router = Router();
+router.get('/', (req, res) => {
+  res.json({ data: Object.keys(json) });
+});
 
-router.use('/', (req: Request, res: Response) => {
+router.use('/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
   const properties = req.path.split('/').filter((q) => q.length);
 
   let data: Record<string, any> = json;
 
+  if (!id || !(json as any)[id]) {
+    throw new BadRequestError();
+  }
 
   if (!properties.length) {
     res.json({response: data})

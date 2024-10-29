cd2408f7f787fda9a3c495dbc2c2051cf89bdd21
Alejandro <alexchuc84@gmail.com>
2024-10-28 23:34:42 -0600

    Add response classes

---
+++ src/core/response.ts
@@ -0,0 +1,29 @@
+import { Response } from "express";
+
+abstract class ApiResponse {
+    status: number;
+
+    constructor(status: number) {
+        this.status = status;
+    }
+
+    public send( res: Response, data: any) {
+        res.status(this.status).json(data);
+    }
+}
+
+export class SuccessResponse extends ApiResponse {
+    constructor() {
+        super(200);
+    }
+
+    public send(res: Response, data: any) {
+        res.status(this.status).json({ success: true, data });
+    }
+}
+
+export class ErrorResponse extends ApiResponse {
+    static send(api: ApiResponse, ress: Response, error: Error) {
+        ress.status(api.status).json({ success: false, error: error.message });
+    }
+}

f597e1c2a4129ed805ff4c8c02b2952097e9128f
Alejandro <alexchuc84@gmail.com>
2024-10-28 23:36:54 -0600

    Modify to make middleware handler and get request

--- src/router/collections.ts
+++ src/router/collections.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from 'express';
 import { BadRequestError, NotFoundError, ServerError } from '../exceptions';
 import { Collections } from '../services';
 import fn from '../utils/async-handler';
+import { CollectionRequest } from '../types/app-request';
+import { SuccessResponse } from '../core/response';
 
 const router = Router();
 
@@ -63,21 +65,55 @@ router.put('/:id', fn(async (req, res) => {
   }
 }));
 
-router.use('/:id', fn(async (req: Request, res: Response) => {
-  if (!['GET'].includes(req.method.toUpperCase())) {
-    throw new NotFoundError();
-  }
+// router.use('/:id', (req, res, next) => {
+//   next();
+// });
 
+
+// type RequestWithId = Request<{ id: string }> & {
+//   id: string;
+//   content: any;
+// };
+
+router.use('/:id', fn(async (req, res, next) => {
   const { id } = req.params;
   const properties = req.path.split('/').filter((q) => q.length);
 
   try {
     let data: any = await Collections.getCollectionById(id, properties);
+    const entries = [
+      ['id', id],
+      ['collection', data],
+      ['properties', properties]
+    ]  as const;
 
-    res.json({ response: data, success: true });
-  } catch {
+    for (const [key, value] of entries) {
+      (req as unknown as CollectionRequest)[key] = value;
+    }
+
+    next();
+  } catch(e) {
     throw new ServerError();
   }
 }));
+
+// router.get(/\/./g, (req: Request, res: Response) => {
+//   console.log(req.path);
+//   (req as unknown as CollectionRequest).coll
+
+//   res.json(['regex']);
+// });
+
+router.get('/:id*', (req: Request, res: Response) => {
+  // Your route handler logic here
+  const { id, collection, properties }  = (req as unknown as CollectionRequest);
+
+  console.log('id *', id, collection, properties);
+
+  console.log('id:', req.params.id);
+
+  return new SuccessResponse().send(res, 'hola mundo');
+})
+
 
 export default router;

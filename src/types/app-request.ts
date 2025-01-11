319e89d360f617d339703e1306e5874ab674408f
Alejandro <alexchuc84@gmail.com>
2025-01-11 00:12:08 -0600

    Add collection type

--- src/types/app-request.ts
+++ src/types/app-request.ts
@@ -1,5 +1,7 @@
+import { Collection } from './collection';
+
 export declare interface CollectionRequest extends Request {
-    collection: any;
+    collection: Collection;
     id: string;
     properties: string[];
 }
